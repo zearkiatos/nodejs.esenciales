356cc7d66cf671a38144b18191e3b653b2ef7c3c
Pedro Capriles <caprilespe@outlook.com>
2020-01-04 23:45:15 -0300

    Add and config mongodb

---
+++ db/connect.js
@@ -0,0 +1,47 @@
+const MongoClient = require("mongodb").MongoClient;
+const config = require("./config");
+const connString = `mongodb://root:root@${config.DB_HOST}:${config.DB_PORT}`;
+
+let instance = null;
+let isDisconnecting = false;
+
+console.log(connString);
+
+module.exports = {
+  connect: () => {
+    return new Promise((resolve, reject) => {
+      MongoClient.connect(connString, { useNewUrlParser: true,useUnifiedTopology: true, authSource: "admin" }, function(
+        err,
+        client
+      ) {
+          console.log(client);
+        if (err) {
+          reject(err);
+        }
+        console.log("Conectado satisfactoriamente al servidor de Mongo! 🟢");
+        instance = client;
+        resolve(client.db(config.DB_NAME));
+      });
+    });
+  },
+  disconnect: () => {
+    if (!instance && !isDisconnecting) {
+      isDisconnecting = true;
+      console.log("Desconectando instancia de Mongo 🔌 🔴");
+      return new Promise((resolve, reject) => {
+        instance.close((err, result) => {
+          if (err) {
+            reject(err);
+            isDisconnecting = false;
+            return;
+          }
+          console.log("Instancia de Mongo desconectada 🔌 🔴");
+          resolve();
+        });
+      });
+    }
+  },
+  instance: () => {
+      return instance;
+  }
+};
--- index.js
+++ index.js
@@ -2,17 +2,37 @@ const express = require("express");
 const logger = require("morgan");
 const bodyParser = require("body-parser");
 const app = express();
+const mongo = require("./db/connect");
 
 app.use(logger("dev"));
 app.use(bodyParser.json());
-app.use(bodyParser.urlencoded({extended: false}));
+app.use(bodyParser.urlencoded({ extended: false }));
 
 require("./routes/views")(app);
 require("./routes/special")(app);
 require("./routes/api")(app);
 
-console.log("Iniciando Express.js 🤖");
+async function initMongo() {
+  const db = await mongo.connect();
+  if (db) {
+    initExpress();
+  }
+}
 
-app.listen(3000,()=>{
+function initExpress() {
+  console.log("Iniciando Express.js 🤖");
+
+  app.listen(3000, () => {
     console.log("Express ha iniciado correctamente! ✅ 🚀");
-})
+    process.on("SIGINT", closeApp);
+    process.on("SIGTERM", closeApp);
+  });
+}
+
+function closeApp() {
+  mongo.disconnect().then(() => {
+    process.exit(0);
+  });
+}
+
+initMongo();
