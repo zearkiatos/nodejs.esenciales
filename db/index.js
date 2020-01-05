c309440cbebbbf7e093642345a1ad8e7a40bb929
Pedro Capriles <caprilespe@outlook.com>
2020-01-05 02:05:58 -0300

    Add connection to mongodb configuration and populate data

---
+++ db/index.js
@@ -0,0 +1,10 @@
+const mongo = require("./connect");
+const {DB_NAME} = require("./config");
+
+module.exports = {
+    postPokemon: function (pokemon) {
+        const db = mongo.instance().db(DB_NAME);
+        const result = db.collection("pokemon").insertOne(pokemon);
+        return result;
+    }
+}
--- routes/api/pokemon.js
+++ routes/api/pokemon.js
@@ -1,7 +1,28 @@
 const API_PATH = "/api";
-const pokemon = require("../../resources/pokemon");
+const pokemons = require("../../resources/pokemon");
+const {postPokemon} = require("../../db");
 module.exports = (app) => {
   app.get(`${API_PATH}/pokemon`, (request, response) => {
-    response.json(pokemon.results);
+    response.json(pokemons.results);
+  });
+
+  app.get(`${API_PATH}/pokemon/:name`, (request, response) => {
+    const name = request.params.name;
+    const pokemon = pokemons.results.find((pokemon) => pokemon.name === name);
+    response.json(pokemon);
+  });
+
+  app.post(`${API_PATH}/pokemon`, async (request, response) => {
+    try{
+        const pokemon = request.body.pokemon;
+        if (pokemon) {
+            const result = await postPokemon(pokemon);
+            return response.json(result);
+        }
+        response.status(400).send({reason: "No pokemon sent."})
+    }
+    catch(err){
+        console.log(err);
+    }
   });
 };
