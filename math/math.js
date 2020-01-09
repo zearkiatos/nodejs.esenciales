6e4afec7178ba4cfb5914900f77ff980d721894a
Pedro Capriles <caprilespe@outlook.com>
2020-01-09 19:57:25 -0300

    Add module and pow functionality into math function

--- math/math.js
+++ math/math.js
@@ -1,7 +1,12 @@
 /* eslint-disable linebreak-style */
+/* eslint no-restricted-properties: [2, {
+    "object": "pow",
+}] */
 module.exports = {
   sum: (a, b) => a + b,
   diff: (a, b) => a - b,
   prod: (a, b) => a * b,
   div: (a, b) => a / b,
+  mod: (a, b) => a % b,
+  pow: (a, b) => Math.pow(a, b),
 };
